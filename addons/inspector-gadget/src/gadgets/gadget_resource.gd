581920db3ff6262a6dfbdb4a535070ecaad5e3fe
Sythelux Rikd <dersyth@gmail.com>
2025-02-27 23:52:08 +0100

    chg: removed inspector Gadget as it is not needed anymore

--- addons/inspector-gadget/src/gadgets/gadget_resource.gd
+++
@@ -1,8 +0,0 @@
-@tool
-extends InspectorGadgetBase
-class_name GadgetResource
-
-var custom_gadget_metadata := {}
-
-func _init(in_node_path: NodePath = NodePath(), in_subnames: String = ""):
-	super._init(in_node_path, in_subnames)
--- addons/inspector-gadget/src/gadgets/gadget_vector4/gadget_quaternion.gd
+++
@@ -1,6 +0,0 @@
-@tool
-extends GadgetVector4
-class_name GadgetQuaternion
-
-static func supports_type(value) -> bool:
-	return value is Quaternion
